47380c0f96ae94625933384f2835c27b87ea2f02
Drarok Ithaqua <source@catchall.drarok.com>
2013-05-10 03:27:52 +0100

    Output the available commands if none passed, and implement an 'all' option.

--- run.php
+++ run.php
@@ -11,20 +11,41 @@
 require_once 'Example/Autoloader.php';
 Example\Autoloader::register();
 
-$commands = array_slice($_SERVER['argv'], 1);
+$requestedExamples = array_slice($_SERVER['argv'], 1);
 
-if (! $commands) {
+$availableExamples = array();
+foreach (glob(__DIR__ . '/Example/Examples/*.php') as $fileName) {
+	$baseName = basename($fileName, '.php');
+
+	// Exclude the abstract class.
+	if ($baseName == 'Base') {
+		continue;
+	}
+
+	$availableExamples[] = $baseName;
+}
+
+if (! $requestedExamples) {
 	echo 'Please pass in one or more example names to run:', PHP_EOL;
 
-	foreach (glob(__DIR__ . '/Example/Examples/*.php') as $fileName) {
-		echo ' * ', basename($fileName, '.php'), PHP_EOL;
+	foreach ($availableExamples as $example) {
+		echo ' * ', $example, PHP_EOL;
 	}
+
+	echo ' * All', PHP_EOL;
 
 	exit(1);
 }
 
-foreach ($commands as $command) {
-	$className = 'Example\\Examples\\' . ucfirst($command);
+// Allow an 'all' option.
+if (count($requestedExamples) == 1
+	&& strtolower($requestedExamples[0]) == 'all'
+) {
+	$requestedExamples = $availableExamples;
+}
+
+foreach ($requestedExamples as $example) {
+	$className = 'Example\\Examples\\' . $example;
 	$instance = new $className();
 	$instance->run();
 	echo '----------', PHP_EOL;
